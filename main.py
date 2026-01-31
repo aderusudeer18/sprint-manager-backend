b44657358c7c282c8541d0755cf8cb8188ec1464
Sudeer Aderu <aderu.sudeer@gmail.com>
2026-01-31 17:43:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # React / Next.js
+    allow_origins=["https://sprint-management-fe.vercel.app/"],  # React / Next.js
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
